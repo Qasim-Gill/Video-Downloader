b606b85c08a9bbaf1ad3eee7e38c232276db8f52
Muhammad Qasim <mq77gill@gmail.com>
2025-01-07 09:22:14 +0500

    Update run.py
    just added comments for better understanding

--- run.py
+++ run.py
@@ -1,10 +1,12 @@
 import yt_dlp
 
 def download_facebook_video(url):
+    # it download best video but it does not care about audio
     # ydl_opts = {
     #     'outtmpl': '%(title)s.%(ext)s',  # Save the video with the title as the filename
     #     'format': 'best',  # Download the best available quality
     # }
+    # it download video with best quality and best audio bcz on fb when i download video it does not download audio so now it is
     ydl_opts = {
         'outtmpl': '%(title)s.%(ext)s',  # Save with title as filename
         'format': 'bestvideo+bestaudio/best',  # Download best video + audio, then merge
@@ -15,7 +17,7 @@ def download_facebook_video(url):
         ydl.download([url])
 
 # Example usage:
-video_url = "https://www.facebook.com/iCodeguru/videos/638822068623256"
+video_url = "https://www.facebook.com/..."
 download_facebook_video(video_url)
 
 # 20
